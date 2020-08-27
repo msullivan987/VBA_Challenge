e999f31045a4c271ce4360400930dfe62876c39e
Michelle Sullivan <michelleecochran@gmail.com>
2020-08-27 16:01:35 -0700

    added total stock volumes to summary table

--- VBA_Challenge_Alpha.vb
+++ VBA_Challenge_Alpha.vb
@@ -1,12 +1,12 @@
 Sub stockSummary()
 
-Dim ticker As String
+Dim tickerName As String
 Dim day As Integer
 Dim openPrice As Double
 Dim highPrice As Double
 Dim lowPrice As Double
 Dim closePrice As Double
-Dim volume As Integer
+Dim tickerVolume As Variant
 
 'Create new summary column headers'
 lastColumn = Cells(1, Columns.Count).End(xlToLeft).Column
@@ -19,16 +19,35 @@ Cells(1, lastColumn + 5).Value = "Total Stock Volume"
 'Find Last Rows of Data Set and Summary Table'
 lastRowData = Cells(Rows.Count, 1).End(xlUp).Row
 
-'Creating Ticker Summary List'
+'Loop through data'
 For i = 2 To lastRowData
 
 lastRowTicker = Cells(Rows.Count, lastColumn + 2).End(xlUp).Row + 1
 
+
+'If end of single ticker's data'
   If Cells(i, 1).Value <> Cells(i + 1, 1).Value Then
-    Cells(lastRowTicker, lastColumn + 2).Value = Cells(i, 1).Value
     
+    'set ticker name'
+    tickerName = Cells(i, 1).Value
+    
+    'add stock volume to total'
+    tickerVolume = tickerVolume + Cells(i, "G").Value
+    
+    
+    'Create ticker summary list'
+    Cells(lastRowTicker, lastColumn + 2).Value = tickerName
+    Cells(lastRowTicker, lastColumn + 5).Value = tickerVolume
+    
+    'Reset ticker volume'
+    tickerVolume = 0
+    
+   Else
+   
+   'add stock volume to total'
+   tickerVolume = tickerVolume + Cells(i, "G").Value
+   
   End If
 Next i
-
 
 End Sub
