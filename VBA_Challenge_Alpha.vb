a82fa1c072bd624861368f97274c5fd56b0f9565
Michelle Sullivan <michelleecochran@gmail.com>
2020-08-28 13:05:46 -0700

    set open and closing prices within the loop

--- VBA_Challenge_Alpha.vb
+++ VBA_Challenge_Alpha.vb
@@ -1,10 +1,7 @@
 Sub stockSummary()
 
 Dim tickerName As String
-Dim day As Integer
 Dim openPrice As Double
-Dim highPrice As Double
-Dim lowPrice As Double
 Dim closePrice As Double
 Dim tickerVolume As Variant
 
@@ -16,16 +13,17 @@ Cells(1, lastColumn + 3).Value = "Yearly Change"
 Cells(1, lastColumn + 4).Value = "Percent Change"
 Cells(1, lastColumn + 5).Value = "Total Stock Volume"
 
-'Find Last Rows of Data Set and Summary Table'
+'Find Last Rows of Data Set'
 lastRowData = Cells(Rows.Count, 1).End(xlUp).Row
+
+'Get opening price for first day of first stock on spreadsheet'
+openPrice = Cells(2,3).Value
 
 'Loop through data'
 For i = 2 To lastRowData
 
 lastRowTicker = Cells(Rows.Count, lastColumn + 2).End(xlUp).Row + 1
 
-
-'If end of single ticker's data'
   If Cells(i, 1).Value <> Cells(i + 1, 1).Value Then
     
     'set ticker name'
@@ -33,7 +31,9 @@ lastRowTicker = Cells(Rows.Count, lastColumn + 2).End(xlUp).Row + 1
     
     'add stock volume to total'
     tickerVolume = tickerVolume + Cells(i, "G").Value
-    
+
+    'set closing price'
+    closePrice = Cells(i,6).Value
     
     'Create ticker summary list'
     Cells(lastRowTicker, lastColumn + 2).Value = tickerName
@@ -41,11 +41,12 @@ lastRowTicker = Cells(Rows.Count, lastColumn + 2).End(xlUp).Row + 1
     
     'Reset ticker volume'
     tickerVolume = 0
+
+    'Set the opening price for the next stock before moving on'
+    openPrice = Cells(i + 1,3).Value
     
    Else
-   
-   'add stock volume to total'
-   tickerVolume = tickerVolume + Cells(i, "G").Value
+    tickerVolume = tickerVolume + Cells(i, "G").Value
    
   End If
 Next i
