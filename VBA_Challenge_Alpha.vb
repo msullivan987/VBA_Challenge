5a9006add94425b91071f05cacb6c51496d596a4
Michelle Sullivan <michelleecochran@gmail.com>
2020-08-29 15:01:33 -0700

    runs on every worksheet

--- VBA_Challenge_Alpha.vb
+++ VBA_Challenge_Alpha.vb
@@ -1,68 +1,73 @@
 Sub stockSummary()
+for each ws in worksheets
 
-Dim tickerName As String
-Dim openPrice As Double
-Dim closePrice As Double
-Dim tickerVolume As Variant
+  Dim tickerName As String
+  Dim openPrice As Double
+  Dim closePrice As Double
+  Dim tickerVolume As Variant
 
-'Create new summary column headers'
+  'Create new summary column headers'
 
-Cells(1, "I").Value = "Ticker"
-Cells(1, "J").Value = "Yearly Change"
-Cells(1, "K").Value = "Percent Change"
-Cells(1, "L").Value = "Total Stock Volume"
+  ws.Cells(1, "I").Value = "Ticker"
+  ws.Cells(1, "J").Value = "Yearly Change"
+  ws.Cells(1, "K").Value = "Percent Change"
+  ws.Cells(1, "L").Value = "Total Stock Volume"
 
-'Find Last Rows of Data Set'
-lastRowData = Cells(Rows.Count, 1).End(xlUp).Row
+  'Find Last Rows of Data Set'
+  lastRowData = ws.Cells(Rows.Count, 1).End(xlUp).Row
 
-'Get opening price for first day of first stock on spreadsheet'
-openPrice = Cells(2,3).Value
+  'Get opening price for first day of first stock on spreadsheet'
+  openPrice = ws.Cells(2,3).Value
 
-'Loop through data'
-For i = 2 To lastRowData
+  'Loop through data'
+  For i = 2 To lastRowData
 
-lastRowTicker = Cells(Rows.Count, "I").End(xlUp).Row + 1
+  lastRowTicker = ws.Cells(Rows.Count, "I").End(xlUp).Row + 1
 
-  If Cells(i, 1).Value <> Cells(i + 1, 1).Value Then
+    If ws.Cells(i, 1).Value <> ws.Cells(i + 1, 1).Value Then
+      
+      'set ticker name'
+      tickerName = ws.Cells(i, 1).Value
+      
+      'add stock volume to total'
+      tickerVolume = tickerVolume + ws.Cells(i, "G").Value
+
+      'set closing price'
+      closePrice = ws.Cells(i,6).Value
+      
+      'Create ticker summary list'
+      ws.Cells(lastRowTicker, "I").Value = tickerName
+      ws.Cells(lastRowTicker, "J").Value = closePrice - openPrice
+      If openPrice <> 0 Then
+        ws.Cells(lastRowTicker, "K").Value = (closePrice - openPrice)/openPrice
+        Else ws.Cells(lastRowTicker, "K").Value = 0
+        End if
+      ws.Cells(lastRowTicker, "L").Value = tickerVolume
+      
+      'Reset ticker volume'
+      tickerVolume = 0
+
+      'Reset the opening price for the next stock before moving on'
+      openPrice = ws.Cells(i + 1,3).Value
+      
+    Else
+      tickerVolume = tickerVolume + ws.Cells(i, "G").Value
     
-    'set ticker name'
-    tickerName = Cells(i, 1).Value
-    
-    'add stock volume to total'
-    tickerVolume = tickerVolume + Cells(i, "G").Value
+    End If
+  Next i
 
-    'set closing price'
-    closePrice = Cells(i,6).Value
-    
-    'Create ticker summary list'
-    Cells(lastRowTicker, "I").Value = tickerName
-    Cells(lastRowTicker, "J").Value = closePrice - openPrice
-    Cells(lastRowTicker, "K").Value = (closePrice - openPrice)/openPrice * 100
-    Cells(lastRowTicker, "L").Value = tickerVolume
-    
-    'Reset ticker volume'
-    tickerVolume = 0
+  'Formatting the summary table'
+  finalTickerRow = ws.Cells(Rows.Count, "I").End(xlUp).Row
 
-    'Reset the opening price for the next stock before moving on'
-    openPrice = Cells(i + 1,3).Value
-    
-   Else
-    tickerVolume = tickerVolume + Cells(i, "G").Value
-   
-  End If
-Next i
+  for i = 2 to finalTickerRow
+    If ws.Cells(i,"J").Value >= 0 Then
+      ws.Cells(i,"J").Interior.ColorIndex = 4
 
-'Formatting the summary table'
-finalTickerRow = Cells(Rows.Count, "I").End(xlUp).Row
+    Else ws.Cells(i,"J").Interior.ColorIndex = 3
 
-for i = 2 to finalTickerRow
-  If Cells(i,"J").Value >= 0 Then
-    Cells(i,"J").Value.Interior.ColorIndex = 4
+    End if
 
-  Else Cells(i,"J").Value.Interior.ColorIndex = 3
-
-  End if
-
-  Cells(i,"K").NumberFormat = "0.00%"
-next i
+    ws.Cells(i,"K").NumberFormat = "0.00%"
+  next i
+next ws  
 End Sub
